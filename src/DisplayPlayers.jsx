fbb7f6709e25dae58145a0e0df1f989aaa37da0e
Jordan <jordanleelong5@gmail.com>
2021-02-05 22:45:38 -0800

    added border and assits per game

--- src/DisplayPlayers.jsx
+++ src/DisplayPlayers.jsx
@@ -4,11 +4,12 @@ function DisplayPlayers({playerList}) {
     console.log(playerList)
     return playerList.map((player, index) => {
         return (
-            <div key={index}>
+            <div key={index} style={{border: 'solid 1px black'}}>
             <h4>{player.team_name}</h4>
-                <p>Player: {player.name}</p>
+                <p>Player: <strong>{player.name}</strong></p>
                 <p>PPG: {player.points_per_game}</p>
                 <p>RPG: {player.rebounds_per_game}</p>
+                <p>APG: {player.assists_per_game}</p>
             </div>
         )
     })
