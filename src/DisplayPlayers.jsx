9df7acf0617cd94f924793c797fd5e9e82454d4c
Jordan <jordanleelong5@gmail.com>
2021-02-05 22:07:16 -0800

    added playername

--- src/DisplayPlayers.jsx
+++ src/DisplayPlayers.jsx
@@ -1,9 +1,11 @@
 import React from 'react'; 
 
 function DisplayPlayers({playerList}) {
+    console.log(playerList)
     return playerList.map((player, index) => {
         return (
             <div key={index}>
+            <h4>{player.team_name}</h4>
                 <p>{player.name}</p>
             </div>
         )
